4f981d6609ef505df6406264d5abec0d9776072f
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-11-16 08:37:00 -0500

    Completion

--- src/main/java/com/pluralsight/streams/Person.java
+++ src/main/java/com/pluralsight/streams/Person.java
@@ -1,2 +1,29 @@
-package com.pluralsight.streams;public class Person {
+package com.pluralsight.streams;
+
+import java.util.List;
+
+public class Person {
+    String firstName, lastName;
+    int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person searchByName (String name, List<Person> people) {
+        for (Person p : people) {
+            if (p.lastName.contains(name) || p.firstName.contains(name)) {
+                return p;
+            }
+        }
+        return null;
+    }
+    public String string() {
+        return "Person{" + "firstName = '" + firstName + '\'' +
+                ", lastName = '" + lastName + '\'' +
+                ", age = " + age +
+                '}';
+    }
 }
--- src/main/java/com/pluralsight/streams/Program.java
+++ src/main/java/com/pluralsight/streams/Program.java
@@ -1,2 +1,63 @@
-package com.pluralsight.streams;public class Program {
+package com.pluralsight.streams;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Jake", "Arnett", 25));
+        personList.add(new Person("Joshua", "Ramos", 21));
+        personList.add(new Person("Randy", "Lopez", 23));
+        personList.add(new Person("Christian", "Crites", 18));
+        personList.add(new Person("Sing", "Tuttanon", 19));
+        personList.add(new Person("Clancy", "Long", 26));
+        personList.add(new Person("Abigiel Woldegerima", " ", 21));
+        personList.add(new Person("Aleksandr", "Nikitin", 20));
+        personList.add(new Person("Caleb", "Howard", 24));
+        personList.add(new Person("Desi", "Rorie", 25));
+        personList.add(new Person("Ivan", "Taverez", 23));
+        personList.add(new Person("Rickey", "Glover", 22));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the first or last name:");
+        String name = scanner.nextLine();
+
+        for (Person p : personList) {
+            if (p.lastName.contains(name) || p.firstName.contains(name)) {
+                System.out.println(p);
+            }
+        }
+        double totalAge = 0;
+
+        for (Person p : personList) {
+            totalAge += p.age;
+        }
+        double averageAge = totalAge / personList.size();
+
+        System.out.println("Average age: " + averageAge);
+
+        Person oldest = personList.get(0);
+        //p = person,
+        //oldest = oldest person
+        for (Person p : personList) {
+            if (p.age > oldest.age) {
+                oldest = p;
+            }
+        }
+        System.out.println("Oldest: " + oldest.firstName + " " + oldest.lastName +
+                ", Age: " + oldest.age);
+
+        Person youngest = personList.get(0);
+
+        for (Person p : personList) {
+            if (p.age < youngest.age) {
+                youngest = p;
+            }
+        }
+
+        System.out.println("Youngest: " + youngest.firstName + " " + youngest.lastName +
+                ", Age: " + youngest.age);
+
+    }
 }
